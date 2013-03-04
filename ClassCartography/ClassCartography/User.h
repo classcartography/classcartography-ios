98498b4b83d531dcbf297a85845a9aab1252d671
Chamara Paul <chamara.paul@gmail.com>
2013-03-04 15:43:37 -0600

    added user object

---
+++ ClassCartography/ClassCartography/User.h
@@ -0,0 +1,13 @@
+//
+//  User.h
+//  ClassCartography
+//
+//  Created by Chamara Paul on 3/4/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@end
---
+++ ClassCartography/ClassCartography/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  ClassCartography
+//
+//  Created by Chamara Paul on 3/4/13.
+//
+//
+
+#import "User.h"
+
+@implementation User
+
+@end
