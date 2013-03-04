02bcbcfa9e0f3ac1949be2003d2a31700c7c2501
Paul Lee <mailpaul@gmail.com>
2013-03-04 18:22:17 -0500

    Adding icons and launch images

--- ClassCartography/ClassCartography/Classes/ViewControllers/TeacherDashboardViewController.h
+++ ClassCartography/ClassCartography/Classes/ViewControllers/TeacherDashboardViewController.h
@@ -7,7 +7,21 @@
 //
 
 #import "DashboardViewController.h"
+#import "PhotoView.h"
+#import "GraphView.h"
+#import "FeedbackView.h"
+#import "CommentView.h"
+#import "NotificationView.h"
 
-@interface TeacherDashboardViewController : DashboardViewController
+@interface TeacherDashboardViewController : DashboardViewController {
+    
+@private
+    IBOutlet PhotoView *photoView;
+    IBOutlet GraphView *graphView;
+    IBOutlet FeedbackView *feedbackView;
+    IBOutlet CommentView *commentView;
+    IBOutlet NotificationView *notificationView;
+}
+
 
 @end
