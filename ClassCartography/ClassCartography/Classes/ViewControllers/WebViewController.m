1a195ac26770683a89ccdddfa48097f4ac4ec426
Chamara Paul <chamara.paul@gmail.com>
2013-03-04 18:57:27 -0600

    adding webviewcontroller

---
+++ ClassCartography/ClassCartography/Classes/ViewControllers/WebViewController.m
@@ -0,0 +1,38 @@
+//
+//  WebViewController.m
+//  ClassCartography
+//
+//  Created by Chamara Paul on 3/4/13.
+//
+//
+
+#import "WebViewController.h"
+
+@interface WebViewController ()
+
+@end
+
+@implementation WebViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
